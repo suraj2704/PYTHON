e1f192ab180302dd725a6a1fd21b9cd696311a27
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-07-05 17:37:58 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+
+A="suraj"
+print(A[0:5:2])
+a='''coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
+Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment.
+ However, some will become seriously ill and require medical attention. 
+ Older people and those with underlying medical conditions like cardiovascular disease, 
+ diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness.
+Anyone can get sick with COVID-19 and become seriously ill or die at any age''' 
+print(len(a))
+print(a.endswith("age"))
+print(a.count("However"))
+# print(a.capitalize())
+print(a.find("any"))
+print(a.replace("sick","suraj"))
+
