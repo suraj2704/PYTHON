5829764565be7e1f4783d2c1b70a00e3d58ce932
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-07-05 17:48:31 +0530

    Add files via upload

---
+++ fruit.py
@@ -0,0 +1,6 @@
+f1=input("enter the first fruit\n")
+f2=input("Enter the second Fruit\n")
+f3=input("Enter the third Fruit\n")
+f4=input("enter the fourth number\n")  
+f_list=[f1, f2, f3, f4]
+print(f_list)
---
+++ tuple.py
@@ -0,0 +1,3 @@
+t=(3, 3, 5, 7, 9, 0)
+print(t.count(3))
+print(t.index(9))
