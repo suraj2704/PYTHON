6f7d89f834a931272cb74273b621e271bc98665d
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-07-07 22:46:15 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+
+list=[10, 4, 6 , 7, 9]
+print(list)
+list[0]=8
+list[3]=80
+print(list)
+print(list[4])
+list[1]=100
+print(list)
+list.sort()
+print(list)
+list.reverse()
+print(list)
+list.append(5)
+print(list)
+list.insert(1,99)
+print(list)
+list.pop(1)
+print(list) 
+list.remove(100)
+print(list)
+f=["vishwa","neel","homelander","ass"]
+print (f[1])
+print (f[2])
