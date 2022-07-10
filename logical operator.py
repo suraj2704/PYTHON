4f8959df7e2991d72e297dbbda1c4d4ca579779e
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-07-10 14:22:56 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,35 @@
+from pickletools import read_int4
+
+
+a=11
+b=5
+
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+
+print("comparision operator in python")
+a=9
+b=10
+print(a==b)
+print(a<b)
+print(a>b)
+print(a!=b)
+
+print('logical operator in python')
+a=6
+b=9
+print(a==b and a>b)
+print(a==b or a<b) 
+print(not(a>b))
+
+
+print(" Assignment Operators in Python")
+c = 9
+d = 34
+
+print(c, d)
+c +=  5
+d += 1
+print(c, d)
