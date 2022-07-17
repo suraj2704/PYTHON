52813bb747d5cdbe6963fd646da2061d4629a5c6
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-07-17 22:54:37 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,10 @@
+r = float (input ("Enter the Radius:\n"))  
+a = 3.1415926535897932*r*r
+c = 2*3.1415926535897932*r
+d = 2*r
+print("The Area of the given circle:",end="")
+print (round(a,3))
+print("The Circumference of circle:",end="")
+print (round(c,3))
+print("The Diameter of Circle:",end="")
+print (round(d,3))
